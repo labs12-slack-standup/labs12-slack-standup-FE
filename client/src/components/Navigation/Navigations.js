f62e6d0f567ff04f27a872bb4d6c2eb56f6737ec
mikaelacurrier <mikaela.currier@gmail.com>
2019-04-26 19:39:28 -0400

    changed to functional component

--- client/src/components/Navigation/Navigations.js
+++ client/src/components/Navigation/Navigations.js
@@ -1,22 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import "./navigation.css";
 
-class Navigation extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-	render() {
-		return (
-			<div className="navigation">
-				<NavLink to="/dashboard/account">Account</NavLink>
-				<NavLink to="/dashboard/reports">Reports</NavLink>
-				<NavLink to="/login">Login</NavLink>
-				<NavLink to="/logout">Logout</NavLink>
-			</div>
-		);
-	}
-}
+const Navigation = () => {
+	// Add modal here?
+	return (
+		<div className="navigation">
+			<NavLink to="/dashboard/account">Account</NavLink>
+			<NavLink to="/dashboard/reports">Reports</NavLink>
+			<NavLink to="/login">Login</NavLink>
+			<NavLink to="/logout">Logout</NavLink>
+		</div>
+	);
+};
 
 export default Navigation;
