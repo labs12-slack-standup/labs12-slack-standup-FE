097d560fa52d9426b8133aa4c1fd4dc967dde646
mikaelacurrier <mikaela.currier@gmail.com>
2019-04-26 17:13:10 -0400

    didn't change these files, needed to add firebase locally and was trying things out

--- client/src/components/Firebase/Firebase.js
+++ client/src/components/Firebase/Firebase.js
@@ -1,29 +1,30 @@
-import axios from 'axios'
-import React from 'react';
-import firebase from 'firebase';
+import axios from "axios";
+import React from "react";
+import firebase from "firebase";
 
 const provider = new firebase.auth.GithubAuthProvider();
 const config = {
-  apiKey: 'AIzaSyCrucsAZVktDATv4zg3QT_FJ20zmg3T39U',
-  authDomain: 'slack-standup-1556221821574.firebaseapp.com'
+	apiKey: "AIzaSyCrucsAZVktDATv4zg3QT_FJ20zmg3T39U",
+	authDomain: "slack-standup-1556221821574.firebaseapp.com"
 };
 firebase.initializeApp(config);
 
-class Firebase extends React.Component{
-  render() {
-    return (
-      <button onClick={this.submit}>GitHub Auth</button>
-    )
-  }
-  submit = async () => {
-    try {
-      const { user } = await firebase.auth().signInWithPopup(provider);
-      await axios.post('https://master-slack-standup.herokuapp.com/api/auth/firebase', user);
-      this.props.history.push('/');
-    } catch (err) {
-      console.log(err);
-    }
-  }
+class Firebase extends React.Component {
+	render() {
+		return <button onClick={this.submit}>GitHub Auth</button>;
+	}
+	submit = async () => {
+		try {
+			const { user } = await firebase.auth().signInWithPopup(provider);
+			await axios.post(
+				"https://master-slack-standup.herokuapp.com/api/auth/firebase",
+				user
+			);
+			this.props.history.push("/");
+		} catch (err) {
+			console.log(err);
+		}
+	};
 }
 
 export default Firebase;
