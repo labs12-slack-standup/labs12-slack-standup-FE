a87e3214f852cfb71e66c69f9377e38726ec2086
mikaelacurrier <mikaela.currier@gmail.com>
2019-04-27 00:12:56 -0400

    ability to add multiple members created (no API calls - all local)

--- client/src/components/Onboarding/Onboarding.js
+++ client/src/components/Onboarding/Onboarding.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./onboarding.css";
 
 class Onboarding extends Component {
 	constructor(props) {
@@ -22,37 +23,49 @@ class Onboarding extends Component {
 		this.setState({ createToggle: !this.state.createToggle });
 		this.setState({ joinToggle: false });
 	};
-	bothToggleOff = e => {};
 	// change handlers
 	changeHandler = e => {
 		this.setState({ [e.target.name]: e.target.value });
 	};
+	// push emails to state when submited
+	// allows the emails to be displayed above the Add Team Member button\
+	// *****Currently does not clear field***** LEVEL 2 BUG
+
 	emailHandler = e => {
 		e.preventDefault();
-		this.state.emails.push(e.target.value);
+		const updatedEmails = [...this.state.emails];
+		updatedEmails.push(this.state.singleEmail);
+		console.log(updatedEmails);
+		this.setState({ emails: updatedEmails });
+		this.setState({ singleEmail: "" });
 	};
 
 	render() {
 		return this.state.createToggle ? (
-			<div>
+			<div className="onboarding">
 				<h3>Create a team:</h3>
+				<h4>Members Added:</h4>
+				{this.state.emails.map((email, idx) => (
+					<div key={idx}>{email}</div>
+				))}
 				<div>Add your team members here by email:</div>
 				<form onSubmit={this.emailHandler}>
 					<input
-						type="text"
+						type="email"
 						// onSubmit={this.emailHandler}
 						name="singleEmail"
 						onChange={this.changeHandler}
 					/>
+					<button onClick={this.emailHandler}>Add Team Member</button>
 				</form>
+				<Link to="dashboard/reports">
+					<button>Create Team</button>
+				</Link>
 				<div>Have a join code?</div>
 				<button onClick={this.joinToggle}>Input Join Code</button>
-				<Link to="dashboard/reports">
-					<button>Let's go!</button>
-				</Link>
 			</div>
 		) : this.state.joinToggle ? (
-			<div>
+			<div className="onboarding">
 				Enter Join Code (provided by your manger):
 				<form>
 					<input
@@ -72,7 +85,7 @@ class Onboarding extends Component {
 				</Link>
 			</div>
 		) : (
-			<div>
+			<div className="onboarding">
 				<h3>Hi! Thanks for signing up.</h3>
 				<h4>Do you have a join code?</h4>
 				<h4>If not, no worries, you can create a team here too</h4>
