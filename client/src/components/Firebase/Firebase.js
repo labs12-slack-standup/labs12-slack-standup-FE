3ae8e702ae5f2b266c16c938b7ede45de64d78ec
shaunmcarmody <shaun.carmody@live.co.uk>
2019-04-30 01:14:56 +0100

    make api call to locatlhost on auth success

--- client/src/components/Firebase/Firebase.js
+++ client/src/components/Firebase/Firebase.js
@@ -1,30 +1,50 @@
 import axios from "axios";
-import React from "react";
+import React, { Component } from "react";
 import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
-const provider = new firebase.auth.GithubAuthProvider();
 const config = {
 	apiKey: process.env.REACT_APP_API_KEY,
 	authDomain: process.env.REACT_APP_AUTH_DOMAIN
 };
 firebase.initializeApp(config);
 
-class Firebase extends React.Component {
-	render() {
-		return <button onClick={this.submit}>GitHub Auth</button>;
-	}
-	submit = async () => {
-		try {
-			const { user } = await firebase.auth().signInWithPopup(provider);
-			await axios.post(
-				"https://master-slack-standup.herokuapp.com/api/auth/firebase",
-				user
-			);
-			this.props.history.push("/onboarding");
-		} catch (err) {
-			console.log(err);
+const uiConfig = {
+	signInFlow: "popup",
+	signInSuccessUrl: "/onboarding",
+	signInOptions: [
+		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+		firebase.auth.GithubAuthProvider.PROVIDER_ID
+	],
+	callbacks: {
+		signInSuccessWithAuthResult: async ({ user }) => {
+			try {
+				const response = await axios.post(
+					// "https://master-slack-standup.herokuapp.com/api/auth/firebase",
+					"http://localhost:5000/api/auth/firebase",
+					user
+				);
+				console.log(response);
+				// localStorage.setItem('token', response.data.token);
+				// this.props.history.push("/onboarding");
+			} catch (err) {
+				console.log(err);
+			}
 		}
-	};
+	}
+};
+
+
+
+class Firebase extends Component {
+	render() {
+		return (
+			<StyledFirebaseAuth
+				uiConfig={uiConfig}
+				firebaseAuth={firebase.auth()}
+			/>
+		)
+	}
 }
 
 export default Firebase;
