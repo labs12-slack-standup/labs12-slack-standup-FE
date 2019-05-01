3566e6c1020ddfaf55ad353bc836795846894a5b
AAsriyan <aasriyan@protonmail.com>
2019-05-01 15:50:18 -0400

    Added authentication config, updated user when creating a team

--- client/src/components/Firebase/Firebase.js
+++ client/src/components/Firebase/Firebase.js
@@ -33,13 +33,13 @@ const uiConfig = {
 				console.log(response);
 				localStorage.setItem('token', response.data);
 				console.log(window.location.pathname);
-				if (window.location.pathname === '/signup') {
-					console.log(window.location.pathname);
-					this.props.history.push('/onboarding');
-				} else {
-					console.log(window.location.pathname);
-					this.props.history.push('/dashboard');
-				}
+				// if (window.location.pathname === '/signup') {
+				// 	console.log(window.location.pathname);
+				// 	this.props.history.push('/onboarding');
+				// } else {
+				// 	console.log(window.location.pathname);
+				// 	this.props.history.push('/dashboard');
+				// }
 			} catch (err) {
 				console.log(err);
 			}
