97412e333e4f35ba130a5a4dc489a4d02068e7c8
AAsriyan <aasriyan@protonmail.com>
2019-05-01 11:05:51 -0400

    Fixing OAuth localstorage for token

--- client/src/components/Firebase/Firebase.js
+++ client/src/components/Firebase/Firebase.js
@@ -1,8 +1,8 @@
-import axios from "axios";
-import React, { Component } from "react";
-import firebase from "firebase";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-
+import axios from 'axios';
+import React, { Component } from 'react';
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import jstz from 'jstz';
 
 const config = {
 	apiKey: process.env.REACT_APP_API_KEY,
@@ -11,8 +11,8 @@ const config = {
 firebase.initializeApp(config);
 
 const uiConfig = {
-	signInFlow: "popup",
-	signInSuccessUrl: "/onboarding",
+	signInFlow: 'popup',
+	signInSuccessUrl: '/onboarding',
 	signInOptions: [
 		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
 		firebase.auth.GithubAuthProvider.PROVIDER_ID
@@ -22,20 +22,30 @@ const uiConfig = {
 			try {
 				const response = await axios.post(
 					// "https://master-slack-standup.herokuapp.com/api/auth/firebase",
-					"http://localhost:5000/api/auth/firebase",
-					user
+					'http://localhost:5000/api/auth/firebase',
+					{
+						user,
+						timezone: jstz
+							.determine()
+							.name()
+					}
 				);
 				console.log(response);
-				// localStorage.setItem('token', response.data.token);
-				// this.props.history.push("/onboarding");
+				localStorage.setItem('token', response.data);
+				console.log(window.location.pathname);
+				if (window.location.pathname === '/signup') {
+					console.log(window.location.pathname);
+					this.props.history.push('/onboarding');
+				} else {
+					console.log(window.location.pathname);
+					this.props.history.push('/dashboard');
+				}
 			} catch (err) {
 				console.log(err);
 			}
 		}
 	}
 };
-
-
 
 class Firebase extends Component {
 	render() {
@@ -44,9 +54,8 @@ class Firebase extends Component {
 				uiConfig={uiConfig}
 				firebaseAuth={firebase.auth()}
 			/>
-		)
+		);
 	}
-
 }
 
 export default Firebase;
