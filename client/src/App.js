bd0a4531102eec9a6a54236fd39d48e5f4165d0d
mikaelacurrier <mikaela.currier@gmail.com>
2019-05-01 19:40:32 -0400

    accounts page updates user info

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Signup from './components/Signup/Signup';
@@ -14,24 +13,6 @@ import ReportResults from './components/Reports/MemberReports/ReportResults';
 import Account from './components/Account/Account';
 import Onboarding from './components/Onboarding/Onboarding';
 import Navigation from './components/Navigation/Navigations';
-=======
-import React from "react";
-import { Route } from "react-router-dom";
-import Signup from "./components/Signup/Signup";
-import Login from "./components/Login/Login";
-import Reports from "./components/Reports/Reports";
-import SingleReport from "./components/Reports/SingleReport";
-import SingleReportMember from "./components/Reports/SingleReportMember";
-import CreateReport from "./components/Reports/ModifyReports/CreateReport";
-import SingleReportManager from "./components/Reports/MangerReports/SingleReportManager";
-import EditReport from "./components/Reports/ModifyReports/EditReport";
-import MemberResponseForm from "./components/Reports/MemberReports/MemberResponseForm";
-import ReportResults from "./components/Reports/MemberReports/ReportResults";
-import Account from "./components/Account/Account";
-import Onboarding from "./components/Onboarding/Onboarding";
-import Navigation from "./components/Navigation/Navigations";
-import ReportInput from "./components/Reports/MemberReports/ReportInput";
->>>>>>> fa68aaf4afb3ff71f5229e377d7cde3a54c0bc9a
 
 import './App.css';
 
@@ -48,16 +29,10 @@ function App() {
 			<Route path="/onboarding" component={Onboarding} />
 
 			{/* REPORT ROUTES */}
-<<<<<<< HEAD
 			<Route
 				path="/dashboard/singlereport"
 				component={SingleReport}
 			/>
-=======
-			<Route exact path="/dashboard/reports" component={Reports} />
-			<Route path="/dashboard/reports/:reportId" component={MemberResponseForm} />
-	
->>>>>>> fa68aaf4afb3ff71f5229e377d7cde3a54c0bc9a
 			{/* MANAGER REPORT VIEWS AND UPDATING */}
 			<Route
 				path="/dashboard/report/manager"
@@ -77,7 +52,6 @@ function App() {
 				path="/dashboard/report/teammember"
 				component={SingleReportMember}
 			/>
-<<<<<<< HEAD
 			<Route
 				path="/dashboard/responseform"
 				component={MemberResponseForm}
@@ -86,14 +60,9 @@ function App() {
 				path="/dashboard/reportresults"
 				component={ReportResults}
 			/>
-=======
-			
-			<Route path="/dashboard/reportresults" component={ReportResults} />
->>>>>>> fa68aaf4afb3ff71f5229e377d7cde3a54c0bc9a
 
 			{/* V IEW FOR SINGLE ACCOUNT */}
 			<Route path="/dashboard/account" component={Account} />
-			
 		</div>
 	);
 }
--- client/src/components/Account/Account.js
+++ client/src/components/Account/Account.js
@@ -1,21 +1,19 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+import axiosWithAuth from '../../config/axiosWithAuth';
 
 class Account extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			accountInfo: []
+			accountInfo: [],
+			newName: '',
+			newPic: ''
 		};
 	}
+
 	componentDidMount() {
-		// call to get account by id
-		// Currently not dynamic - must come back when Auth is finalized
-		this.getAccount(1);
-	}
-	getAccount = id => {
-		const endpoint = `https://master-slack-standup.herokuapp.com/api/users/${id}`;
-		axios
+		const endpoint = `https://master-slack-standup.herokuapp.com/api/users/byuser`;
+		axiosWithAuth()
 			.get(endpoint)
 			.then(res =>
 				this.setState({
@@ -23,18 +21,81 @@ class Account extends Component {
 				})
 			)
 			.catch(err => console.log(err));
+	}
+
+	updateUser = e => {
+		e.preventDefault();
+		const endpoint = `https://master-slack-standup.herokuapp.com/api/users/`;
+		const editedUser = {};
+		if (this.state.newName) {
+			editedUser.fullName = this.state.newName;
+		}
+		if (this.state.newPic) {
+			editedUser.profilePic = this.state.newPic;
+		}
+		console.log(editedUser);
+		axiosWithAuth()
+			.put(endpoint, editedUser)
+			.then(res => {
+				console.log(res);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	};
+
+	changeHandler = e => {
+		this.setState({ [e.target.name]: e.target.value });
 	};
 
 	render() {
-		return this.state.accountInfo.roles === "manager" ? (
+		return this.state.accountInfo.roles === 'admin' ? (
 			<div>MANAGER VIEW</div>
 		) : (
 			<div>
 				Account Info:
 				<div>
-					<div>Email: {this.state.accountInfo.email}</div>
-					<div>TeamId: {this.state.accountInfo.teamId}</div>
+					<div>
+						Email:{' '}
+						{this.state.accountInfo.email}
+					</div>
+					<div>
+						TeamId:{' '}
+						{this.state.accountInfo.teamId}
+					</div>
+					<div>
+						Full Name:{' '}
+						{
+							this.state.accountInfo
+								.fullName
+						}
+					</div>
+					<img
+						src={
+							this.state.accountInfo
+								.profilePic
+						}
+					/>
 				</div>
+				<form onSubmit={this.updateUser}>
+					<input
+						type="text"
+						value={this.state.newName}
+						name="newName"
+						onChange={this.changeHandler}
+						placeholder="What's your name"
+					/>
+					<input
+						type="text"
+						value={this.state.newPic}
+						name="newPic"
+						placeholder="gimme a picture link"
+						onChange={this.changeHandler}
+					/>
+					<button type="submit">
+						Submit Changes
+					</button>
+				</form>
 			</div>
 		);
 	}
