c009cd66d66a248dc189ce74cadf2c7da172bbc9
mikaelacurrier <mikaela.currier@gmail.com>
2019-05-01 19:19:37 -0400

    small change because confusion

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import Signup from "./components/Signup/Signup";
-import Login from "./components/Login/Login";
-import Reports from "./components/Reports/Reports";
-import SingleReport from "./components/Reports/SingleReport";
-import SingleReportMember from "./components/Reports/SingleReportMember";
-import CreateReport from "./components/Reports/ModifyReports/CreateReport";
-import SingleReportManager from "./components/Reports/MangerReports/SingleReportManager";
-import EditReport from "./components/Reports/ModifyReports/EditReport";
-import MemberResponseForm from "./components/Reports/MemberReports/MemberResponseForm";
-import ReportResults from "./components/Reports/MemberReports/ReportResults";
-import Account from "./components/Account/Account";
-import Onboarding from "./components/Onboarding/Onboarding";
-import Navigation from "./components/Navigation/Navigations";
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Signup from './components/Signup/Signup';
+import Login from './components/Login/Login';
+import Reports from './components/Reports/Reports';
+import SingleReport from './components/Reports/SingleReport';
+import SingleReportMember from './components/Reports/SingleReportMember';
+import CreateReport from './components/Reports/ModifyReports/CreateReport';
+import SingleReportManager from './components/Reports/MangerReports/SingleReportManager';
+import EditReport from './components/Reports/ModifyReports/EditReport';
+import MemberResponseForm from './components/Reports/MemberReports/MemberResponseForm';
+import ReportResults from './components/Reports/MemberReports/ReportResults';
+import Account from './components/Account/Account';
+import Onboarding from './components/Onboarding/Onboarding';
+import Navigation from './components/Navigation/Navigations';
 
-import "./App.css";
+import './App.css';
 
 function App() {
 	return (
@@ -31,21 +31,39 @@ function App() {
 			<Route path="/dashboard/reports" component={Reports} />
 			{/* SINGLE REPORT NOT NEEDED - ABSTRACT AWAY WITH A REROUTE */}
 			{/* REPORT ROUTES */}
-			<Route path="/dashboard/singlereport" component={SingleReport} />
+			<Route
+				path="/dashboard/singlereport"
+				component={SingleReport}
+			/>
 			{/* MANAGER REPORT VIEWS AND UPDATING */}
-			<Route path="/dashboard/report/manager" component={SingleReportManager} />
-			<Route path="/dashboard/createreport" component={CreateReport} />
-			<Route path="/dashboard/editreport" component={EditReport} />
+			<Route
+				path="/dashboard/report/manager"
+				component={SingleReportManager}
+			/>
+			<Route
+				path="/dashboard/createreport"
+				component={CreateReport}
+			/>
+			<Route
+				path="/dashboard/editreport"
+				component={EditReport}
+			/>
 
 			{/* TEAM MEMBER VIEWS AND UPDATING */}
 			<Route
 				path="/dashboard/report/teammember"
 				component={SingleReportMember}
 			/>
-			<Route path="/dashboard/responseform" component={MemberResponseForm} />
-			<Route path="/dashboard/reportresults" component={ReportResults} />
+			<Route
+				path="/dashboard/responseform"
+				component={MemberResponseForm}
+			/>
+			<Route
+				path="/dashboard/reportresults"
+				component={ReportResults}
+			/>
 
-			{/* VIEW FOR SINGLE ACCOUNT */}
+			{/* V IEW FOR SINGLE ACCOUNT */}
 			<Route path="/dashboard/account" component={Account} />
 		</div>
 	);
--- client/src/components/Reports/SingleReport.js
+++ client/src/components/Reports/SingleReport.js
@@ -1,6 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 //firebase sending back a user object from auth - where are we storing that? Write a ternary based on user.role:
 // user.role === manager ? show manager view : show teamMember view
@@ -8,7 +7,7 @@ import { Link } from "react-router-dom";
 //how are we accessing questions? User clicks into this report, if they're a team member they should be able to answer the questions that are provided. Does this need to be a stateful component? And if we query the questions table, how do we avoid populating with ALL questions that have this report id? I think we need an endpoint to make a query that says this:
 // db('questions').where(reportId=reportId and answer = null)
 
-const SingleReport = (props) => {
+const SingleReport = props => {
 	return (
 		<div>
 			<h2>Report Name: {props.report.reportName}</h2>
@@ -17,6 +16,5 @@ const SingleReport = (props) => {
 		</div>
 	);
 };
-
 
 export default SingleReport;
