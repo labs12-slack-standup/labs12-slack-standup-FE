5c929809b8cda1ae7a821596f98922c1086c1b2f
AAsriyan <aasriyan@protonmail.com>
2019-05-03 18:08:23 -0400

    Team fixed CRUD operations on the front end for reports/responses

--- client/src/components/Reports/MemberReports/ReportResults.js
+++ client/src/components/Reports/MemberReports/ReportResults.js
@@ -1,39 +1,37 @@
-import React, { Component} from "react";
-import { axiosWithAuth } from '../../../config/axiosWithAuth';
+import React, { Component } from 'react';
+import { axiosWithAuth, baseURL } from '../../../config/axiosWithAuth';
 
 class ReportResults extends Component {
 	state = {
 		responses: []
-	}
+	};
 
 	render() {
+		// console.log(this.state.responses);
+		if (!this.state.responses || this.state.responses.length < 1) {
+			return <div>No Responses.</div>;
+		}
 		return (
 			<div>
-				{
-					this.state.responses.map(response => (
-						<div key={response.userId}>
-							<h3>{response.fullName}</h3>
-							{
-								response.questions.map(({question, answer}) => (
-									<div>
-										<p>{question}</p>
-										<p>{answer}</p>
-									</div>
-								))
-							}
-						</div>
-					))
-				}
+				{this.state.responses.map(response => (
+					<div key={response.userId}>
+						<h3>{response.fullName}</h3>
+						{response.questions.map(({ question, answer }) => (
+							<div>
+								<p>{question}</p>
+								<p>{answer}</p>
+							</div>
+						))}
+					</div>
+				))}
 			</div>
-		)
+		);
 	}
 
 	componentDidMount() {
 		axiosWithAuth()
-			.get(`http://localhost:5000/api/responses/${this.props.match.params.reportId}`)
-			.then(res =>
-				this.setState({ responses: res.data.membersArray })
-			)
+			.get(`${baseURL}/responses/${this.props.match.params.reportId}`)
+			.then(res => this.setState({ responses: res.data.membersArray }))
 			.catch(err => console.log(err));
 	}
 }
--- client/src/components/Reports/SingleReport.js
+++ client/src/components/Reports/SingleReport.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
-
 const SingleReport = props => {
+	console.log(props.report.id);
 	return (
 		<div>
 			<h2>Report Name: {props.report.reportName}</h2>
 			<h3>Report Created: {props.report.created_at}</h3>
 			<h4>Report Message: {props.report.message}</h4>
 			<Link to={`/dashboard/reports/${props.report.id}`}>Respond</Link>
-			<br/>
-			<Link to={`/dashboard/editreport/${props.report.id}`}>Edit</Link> 
-      <br />
-      <button onClick={props.deleteReport}>Delete</button> 
-
+			<br />
+			<Link to={`/dashboard/editreport/${props.report.id}`}>Edit</Link>
+			<br />
+			<button onClick={() => props.deleteReport(props.report.id)}>
+				Delete
+			</button>
 		</div>
 	);
 };
