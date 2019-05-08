02250d5a00b5ab1fdc42d9a62ae595e3c87b4703
shaunmcarmody <shaun.carmody@live.co.uk>
2019-05-08 19:19:43 +0100

    render updated list of reports on delete

--- client/src/components/Reports/Reports.js
+++ client/src/components/Reports/Reports.js
@@ -4,7 +4,6 @@ import { axiosWithAuth, baseURL } from '../../config/axiosWithAuth';
 import Slack from '../Slack/Slack';
 
 import { Link } from 'react-router-dom';
-// SAME AS SURVEY LIST ON WIREFRAME
 
 class Reports extends Component {
 	state = {
@@ -25,51 +24,46 @@ class Reports extends Component {
 			)
 			.catch(err => console.log(err));
 	}
+	
 	deleteReport = id => {
 		const endpoint = `${baseURL}/reports/${id}`;
 
 		axiosWithAuth()
 			.delete(endpoint)
-			.then(res => console.log(res))
+			.then(res => 
+				this.setState({
+					message: res.data.message,
+					reports: res.data.reports
+				})
+			)
 			.catch(err => console.log(err));
 	};
 
 	render() {
-		if (this.state.reports.length < 1) {
-			return (
-				<div>
-					<h2>You have not created any reports</h2>
+		return (
+			<>
+				<h3>Reports</h3>
+				<section>
+					<Slack />
 					<Link to="/dashboard/createreport">
 						<button>Create Report</button>
 					</Link>
-					<Slack />
-				</div>
-			);
-		}
-		return (
-			<div>
-				Reports:
-				<div>
-					<Slack />
-					{/* passing reports from state to individual components */}
-					{this.state.reports.map(report => (
+				</section>
+				{
+					this.state.reports < 1 ?
+					<div>
+						<h2>You have not created any reports</h2>
+					</div> :
+					this.state.reports.map(report => (
 						<SingleReport
 							key={report.id}
 							report={report}
 							deleteReport={this.deleteReport}
 						/>
-					))}
-					<Link to="/dashboard/createreport">
-						<button>Create New Report</button>
-					</Link>
-					<h2>This component needs access to:</h2>
-					<ul>
-						<li>QUERY: all reports by teamId</li>
-						<li>Report Name, schedule, team member list,</li>
-					</ul>
-				</div>
-			</div>
-		);
+					))
+				}
+			</>
+		)
 	}
 }
 
