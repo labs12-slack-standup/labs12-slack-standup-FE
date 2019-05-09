36f4de1dd4e866365d7ece2566e7000419ac8706
mikaelacurrier <mikaela.currier@gmail.com>
2019-05-09 13:30:54 -0400

    admin can view archived reports and reactivate them

--- client/src/components/Account/Account.js
+++ client/src/components/Account/Account.js
@@ -7,7 +7,8 @@ class Account extends Component {
 		this.state = {
 			accountInfo: [],
 			newName: '',
-			newPic: ''
+			newPic: '',
+			achivedReports: []
 		};
 	}
 
@@ -22,6 +23,30 @@ class Account extends Component {
 			)
 			.catch(err => console.log(err));
 	}
+	viewAchivedReports = () => {
+		const endpoint = `${baseURL}/reports`;
+		axiosWithAuth()
+			.get(endpoint)
+			.then(res =>
+				this.setState({
+					achivedReports: res.data.reports
+				})
+			)
+			.catch(err => console.log(err));
+	};
+	reactivateReport = id => {
+		const editedReport = {
+			active: true
+		};
+
+		console.log(editedReport);
+		const endpoint = `${baseURL}/reports/${id}`;
+		axiosWithAuth()
+			.put(endpoint, editedReport)
+			.then(res => console.log(res))
+			.catch(err => console.log(err));
+		this.props.history.push('/dashboard/reports');
+	};
 
 	updateUser = e => {
 		e.preventDefault();
@@ -61,10 +86,54 @@ class Account extends Component {
 	};
 
 	render() {
-		// return this.state.accountInfo.roles === 'admin' ? (
-		// 	<div>MANAGER VIEW</div>
-		// ) : (
-		return (
+		const inactiveReports = this.state.achivedReports.filter(
+			report => !report.active
+		);
+		return this.state.accountInfo.roles === 'admin' ? (
+			<div>
+				MANAGER VIEW Account Info:
+				<div>
+					<div>Email: {this.state.accountInfo.email}</div>
+					<div>Join Code: {this.state.accountInfo.joinCode}</div>
+					<div>Full Name: {this.state.accountInfo.fullName}</div>
+
+					<img
+						src={this.state.accountInfo.profilePic}
+						alt="a headshot, preferably"
+					/>
+				</div>
+				<form onSubmit={this.updateUser}>
+					<input
+						type="text"
+						value={this.state.newName}
+						name="newName"
+						onChange={this.changeHandler}
+						placeholder="What's your name"
+					/>
+					<input
+						type="text"
+						value={this.state.newPic}
+						name="newPic"
+						placeholder="gimme a picture link"
+						onChange={this.changeHandler}
+					/>
+					<button type="submit">Submit Changes</button>
+					<div />
+				</form>
+				<button onClick={this.viewAchivedReports}>View Archived Reports</button>
+				<div>This will be an accordian with styling</div>
+				<div>
+					{inactiveReports.map((report, idx) => (
+						<div key={idx}>
+							<h3>{report.reportName}</h3>
+							<button onClick={() => this.reactivateReport(report.id)}>
+								Reactivate Report
+							</button>
+						</div>
+					))}
+				</div>
+			</div>
+		) : (
 			<div>
 				Account Info:
 				<div>
