91b04cbb650d443093d362981a1d19329fbf9dab
mikaelacurrier <mikaela.currier@gmail.com>
2019-05-09 15:07:22 -0400

    archive button added to reports - needs debugging because the user has to refresh page

--- client/src/components/Dashboard/ReportsDash.js
+++ client/src/components/Dashboard/ReportsDash.js
@@ -29,13 +29,15 @@ class ReportsDash extends Component {
 			.catch(err => console.log(err));
 	};
 
-	deleteReport = id => {
+	archiveReport = id => {
 		const endpoint = `${baseURL}/reports/${id}`;
-
+		const updatedReport = {
+			active: false
+		};
 		axiosWithAuth()
-			.delete(endpoint)
+			.put(endpoint, updatedReport)
 			.then(res => {
-				this.setResponseAsState(res.data.reports);
+				//this.setResponseAsState(res.data.reports);
 			})
 			.catch(err => console.log(err));
 	};
@@ -44,8 +46,8 @@ class ReportsDash extends Component {
 		console.log(reports);
 		this.setState({
 			reports: reports
-		})
-	}
+		});
+	};
 
 	render() {
 		return (
@@ -58,7 +60,7 @@ class ReportsDash extends Component {
 							<Reports
 								{...props}
 								reports={this.state.reports}
-								deleteReport={this.deleteReport}
+								archiveReport={this.archiveReport}
 							/>
 						)}
 					/>
@@ -66,7 +68,10 @@ class ReportsDash extends Component {
 						exact
 						path="/dashboard/reports/new"
 						render={props => (
-							<CreateReport {...props} setResponseAsState={this.setResponseAsState} />
+							<CreateReport
+								{...props}
+								setResponseAsState={this.setResponseAsState}
+							/>
 						)}
 					/>
 					<Route
--- client/src/components/Reports/Reports.js
+++ client/src/components/Reports/Reports.js
@@ -4,12 +4,8 @@ import Slack from '../Slack/Slack';
 import { Link } from 'react-router-dom';
 
 class Reports extends Component {
-	activeReports = this.props.reports.map(report => report);
-	//active = for(i=0;i<{this.props.reports.length};i++)
 	render() {
 		const activeReports = this.props.reports.filter(report => report.active);
-		console.log(activeReports);
-		console.log(this.props.reports);
 		if (this.props.reports.length < 1) {
 			return (
 				<div>
@@ -34,7 +30,7 @@ class Reports extends Component {
 						<SingleReport
 							key={report.id}
 							report={report}
-							deleteReport={this.props.deleteReport}
+							archiveReport={this.props.archiveReport}
 						/>
 					))}
 				</div>
