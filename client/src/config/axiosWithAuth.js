f2b03a67ed8f152ed00b971dd0054313efe8ca4d
Erin Koen <erin.koen@gmail.com>
2019-05-10 12:04:46 -0400

    slack redirect url points to deployed

--- client/src/config/axiosWithAuth.js
+++ client/src/config/axiosWithAuth.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-export const baseURL = 'https://master-slack-standup.herokuapp.com/api';
-// export const baseURL = 'http://localhost:5000/api';
+// export const baseURL = 'https://master-slack-standup.herokuapp.com/api';
+export const baseURL = 'http://localhost:5000/api';
+export const deployedFEURL = 'https://stand-em-ups.netlify.com'
+export const localFEURL = 'http://localhost:3000'
 
 export function axiosWithAuth() {
 	const token = localStorage.getItem('token');
