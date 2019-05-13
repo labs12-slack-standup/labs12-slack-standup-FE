cf3ca52282bf901ef90a2fbb228eb095c029af79
mikaelacurrier <mikaela.currier@gmail.com>
2019-05-13 15:35:45 -0400

    blueprintjs up and running - nav styling begun

--- client/src/components/Navigation/Navigations.js
+++ client/src/components/Navigation/Navigations.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './navigation.css';
+import {
+	Navbar,
+	Button,
+	Classes,
+	NavbarGroup,
+	NavbarDivider
+} from '@blueprintjs/core';
 
 class Navigation extends React.Component {
 	// Add modal here?
@@ -12,14 +19,40 @@ class Navigation extends React.Component {
 	};
 	render() {
 		return (
-			<div className="navigation">
-				<NavLink to="/dashboard/account">Account</NavLink>
-				<NavLink to="/dashboard">Dashboard</NavLink>
-				<NavLink to="/login">Login</NavLink>
-				<NavLink to="/login" onClick={this.handleLogout}>
-					Logout
-				</NavLink>
-			</div>
+			<Navbar>
+				<NavbarGroup>
+					<Navbar.Heading>Stand-Em-Ups</Navbar.Heading>
+					<NavbarDivider />
+					<NavLink to="/dashboard/account">
+						<Button className={Classes.MINIMAL} text="Account" />
+					</NavLink>
+					<NavLink to="/dashboard">
+						<Button className={Classes.MINIMAL} text="Dashboard" />
+					</NavLink>
+					<NavLink to="/login">
+						<Button className={Classes.MINIMAL} text="Login" />
+					</NavLink>
+					<NavLink to="/login" onClick={this.handleLogout}>
+						<Button className={Classes.MINIMAL} text="Logout" />
+					</NavLink>
+				</NavbarGroup>
+			</Navbar>
+			// <nav class="bp3-navbar .modifier">
+			// 	<div class="bp3-navbar-group bp3-align-left">
+			// 		<div class="bp3-navbar-heading">Blueprint</div>
+			// 		<input class="bp3-input" placeholder="Search files..." type="text" />
+			// 	</div>
+			// 	<div class="bp3-navbar-group bp3-align-right">
+			// 		<button class="bp3-button bp3-minimal bp3-icon-home">Home</button>
+			// 		<button class="bp3-button bp3-minimal bp3-icon-document">
+			// 			Files
+			// 		</button>
+			// 		<span class="bp3-navbar-divider" />
+			// 		<button class="bp3-button bp3-minimal bp3-icon-user" />
+			// 		<button class="bp3-button bp3-minimal bp3-icon-notifications" />
+			// 		<button class="bp3-button bp3-minimal bp3-icon-cog" />
+			// 	</div>
+			//</nav>
 		);
 	}
 }
--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+
 // const client = new ApolloClient({
 //   uri: 'http://localhost:4000/',
 //   headers: {
