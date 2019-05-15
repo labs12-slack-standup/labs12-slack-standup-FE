869b046e938173ac92e5173255cd0284e0f62af3
mikaelacurrier <mikaela.currier@gmail.com>
2019-05-15 09:00:51 -0400

    more styling, mostly to onboarding

--- client/src/components/Onboarding/CreateTeam.js
+++ client/src/components/Onboarding/CreateTeam.js
@@ -4,18 +4,20 @@ import { EditableText, TextArea, Intent } from '@blueprintjs/core';
 const CreateTeam = props => {
 	return (
 		<div className="onboarding">
-			<h3>Create a team:</h3>
+			<h3>Add team member's email</h3>
 			<TextArea
 				style={{ width: 400 + 'px', height: 100 + 'px' }}
 				className="createTeamEmail"
 				intent={Intent.PRIMARY}
-				placeholder="Add team members' emails, separated by commas"
+				placeholder="Emails, separated by commas"
 				value={props.emails}
 				onChange={props.changeHandler}
 				name="emails"
 			/>
-			<button onClick={props.createTeam}>Create Team</button>
-			{/* </Link> */}
+			<button onClick={props.createTeam} style={{ marginBottom: 100 + 'px' }}>
+				Create Team
+			</button>
+
 			<div>Have a join code?</div>
 			<button onClick={props.joinToggle}>Input Join Code</button>
 			<button onClick={props.toggleAllOff}>Cancel</button>
