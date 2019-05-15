52dd1f33b58af1e2d23b331ab609d22ffea4023b
mikaelacurrier <mikaela.currier@gmail.com>
2019-05-15 10:02:41 -0400

    axios fix

--- client/src/config/axiosWithAuth.js
+++ client/src/config/axiosWithAuth.js
@@ -4,7 +4,7 @@ export const baseURL = 'https://master-slack-standup.herokuapp.com/api';
 // export const baseURL = 'http://localhost:4000/api';
 
 //Slack login redirects to a URL, changed to local for dev
-export const slackURL = 'https://stand-em-ups.netlify.com/slack/auth/';
+export const slackURL = 'https://stand-em-ups.netlify.com/slack/auth';
 // export const slackURL = 'http://localhost:3000/slack/auth/';
 
 export function axiosWithAuth() {
