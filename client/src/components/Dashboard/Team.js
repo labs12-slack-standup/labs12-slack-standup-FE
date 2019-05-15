3d17941da17d6b2e20e407ff98e16695a69d0305
mikaelacurrier <mikaela.currier@gmail.com>
2019-05-15 10:12:43 -0400

    team.js changed to class component so props needs this keyword

--- client/src/components/Dashboard/Team.js
+++ client/src/components/Dashboard/Team.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import { Card, Elevation, Button } from '@blueprintjs/core';
 import User from './User';
 
-
 class Team extends Component {
 	state = {
 		openInactiveUsers: false
@@ -13,37 +12,36 @@ class Team extends Component {
 		this.setState({ openInactiveUsers: !this.state.openInactiveUsers });
 	};
 
-
 	render() {
 		const activeUsers = this.props.users.filter(user => user.active);
 		const inactiveUsers = this.props.users.filter(user => !user.active);
-		
-	return (
-		<div>
-			<h3>Teamies:</h3>
-			<h4>Active Users on Team</h4>
-			{activeUsers.map(user => (
-				<User
-					user={user}
-					key={user.id}
-					activateUser={props.activateUser}
-					deactivateUser={props.deactivateUser}
-				/>
-			))}
-			<br />
-			<br />
-			<h4>Inactive Users</h4>
-			{inactiveUsers.map(user => (
-				<User
-					user={user}
-					key={user.id}
-					activateUser={props.activateUser}
-					deactivateUser={props.deactivateUser}
-				/>
-			))}
-		</div>
-	);
-};
 
+		return (
+			<div>
+				<h3>Teamies:</h3>
+				<h4>Active Users on Team</h4>
+				{activeUsers.map(user => (
+					<User
+						user={user}
+						key={user.id}
+						activateUser={this.props.activateUser}
+						deactivateUser={this.props.deactivateUser}
+					/>
+				))}
+				<br />
+				<br />
+				<h4>Inactive Users</h4>
+				{inactiveUsers.map(user => (
+					<User
+						user={user}
+						key={user.id}
+						activateUser={this.props.activateUser}
+						deactivateUser={this.props.deactivateUser}
+					/>
+				))}
+			</div>
+		);
+	}
+}
 
 export default Team;
