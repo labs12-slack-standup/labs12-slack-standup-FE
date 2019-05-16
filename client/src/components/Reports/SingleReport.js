89082ff9a81b985594e64f4c378fc31f536961dc
Erin Koen <erin.koen@gmail.com>
2019-05-16 19:21:53 -0400

    only admin can edit or archive reports, changed the date display on SingleReport.js

--- client/src/components/Reports/SingleReport.js
+++ client/src/components/Reports/SingleReport.js
@@ -1,24 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Card, Button } from '@blueprintjs/core';
+import { Card, Button, Icon } from '@blueprintjs/core';
+import { iconClass } from '@blueprintjs/core/lib/esm/common/classes';
+
 
 const SingleReport = props => {
+	const week = [
+		'Monday',
+		'Tueday',
+		'Wednesday',
+		'Thursday',
+		'Friday',
+		'Saturday',
+		'Sunday'
+	];
+
 	return (
 		<Card className="reportsCard">
+			{console.log(props.report)}
 			<Link
 				to={`/dashboard/reports/${props.report.id}`}
 				style={{ textDecoration: 'none' }}
 			>
-				<h2>{props.report.reportName}</h2>
-				<h4>Message: {props.report.message}</h4>
-				<h5>{props.report.created_at}</h5>
+				<h1>{props.report.reportName}</h1>
+				<h4>Scheduled days for delivery:</h4>
+				<div>
+					<Icon icon="timeline-events" />
+					<section className="days-flex">
+						{week.map(day => (
+							<div
+								key={day}
+								className={`day ${
+									props.report.schedule.includes(day) ? 'selected' : ''
+								}`}
+							>
+								{day.charAt(0)}
+							</div>
+						))}
+					</section>
+				</div>
 			</Link>
-			<Button className={props.role !== 'admin' ? 'bp3-disabled' : null}>
-				<Link to={`/dashboard/reports/${props.report.id}/edit`}>Edit</Link>
-			</Button>
+
+			<Link
+				to={`/dashboard/reports/${props.report.id}/edit`}
+				className={props.role !== 'admin' ? 'disabled-link' : ''}
+			>
+				<Button className={props.role !== 'admin' ? 'bp3-disabled' : null}>
+					Edit{' '}
+				</Button>
+			</Link>
 			<Button
 				onClick={() => props.archiveReport(props.report.id)}
-				className={props.role !== 'admin' ? 'bp3-disabled' : null}
+				className={props.role !== 'admin' ? 'disabled-link' : null}
 			>
 				Archive
 			</Button>
