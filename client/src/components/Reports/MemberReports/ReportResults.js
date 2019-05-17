fa8a8308f78bc1957cc65e89e0f600cd51077321
AAsriyan <aasriyan@protonmail.com>
2019-05-17 09:54:34 -0400

    Fixed responses to block a user from responding to a report multiple times
    Co-authored-by: Erin Koen<erin.koen@gmail.com>

--- client/src/components/Reports/MemberReports/ReportResults.js
+++ client/src/components/Reports/MemberReports/ReportResults.js
@@ -4,12 +4,14 @@ import styled from 'styled-components';
 import { Card, Elevation } from '@blueprintjs/core';
 import MemberResponseForm from './MemberResponseForm';
 import { axiosWithAuth, baseURL } from '../../../config/axiosWithAuth';
+import jwt_decode from 'jwt-decode';
 import './ReportResults.css';
 
 class ReportResults extends Component {
 	state = {
 		responses: [],
-		clickedDate: null
+		clickedDate: null,
+		filteredResponse: []
 	};
 
 	render() {
@@ -19,18 +21,37 @@ class ReportResults extends Component {
 			month: 'long',
 			day: 'numeric'
 		};
+
 		return (
 			<main className="report-results-container">
 				<section className="report-results-aside">
-					<Card interactive={true} elevation={Elevation.TWO}>
-						<MemberResponseForm
-							{...this.props}
-							updateWithUserResponse={this.updateWithUserResponse}
-						/>
-					</Card>
-					<Card interactive={true} elevation={Elevation.TWO} style={{ marginTop: '30px' }}>
+					{this.state.filteredResponse.length > 0 ? (
+						<Card
+							interactive={false}
+							elevation={Elevation.TWO}
+							className="completed-report"
+						>
+							<h3>Thank you for filling out this report!</h3>
+						</Card>
+					) : (
+						<Card interactive={false} elevation={Elevation.TWO}>
+							<MemberResponseForm
+								{...this.props}
+								updateWithUserResponse={this.updateWithUserResponse}
+							/>
+						</Card>
+					)}
+
+					<Card
+						interactive={false}
+						elevation={Elevation.TWO}
+						style={{ marginTop: '30px' }}
+					>
 						<h1 className="report-results-filter">Filter by day</h1>
-						<DatePicker getByDate={this.getByDate} clickedDate={this.state.clickedDate} />
+						<DatePicker
+							getByDate={this.getByDate}
+							clickedDate={this.state.clickedDate}
+						/>
 					</Card>
 				</section>
 				<section className="report-results-feed">
@@ -43,25 +64,30 @@ class ReportResults extends Component {
 											.toLocaleDateString('en-US', options)
 											.replace(',', '')}
 									</h3>
-									{
-										batch.responses.map(response => (
-											<div key={response.userId} className="response-container">
-												<img className="response-container-image" src={response.profilePic} alt={response.fullName} />
-												<div className="response-container-main">
-													<h3 className="response-container-main-name">{response.fullName}</h3>
-													{
-														response.questions.map(({ question, answer, id }) => (
-															<div key={id}>
-																<h6 className="response-container-main-question">{question}</h6>
-																<p className="response-container-main-answer">{answer}</p>
-															</div>
-														))
-													}
-												</div>
+									{batch.responses.map(response => (
+										<div key={response.userId} className="response-container">
+											<img
+												className="response-container-image"
+												src={response.profilePic}
+												alt={response.fullName}
+											/>
+											<div className="response-container-main">
+												<h3 className="response-container-main-name">
+													{response.fullName}
+												</h3>
+												{response.questions.map(({ question, answer, id }) => (
+													<div key={id}>
+														<h6 className="response-container-main-question">
+															{question}
+														</h6>
+														<p className="response-container-main-answer">
+															{answer}
+														</p>
+													</div>
+												))}
 											</div>
-										))
-									}
-
+										</div>
+									))}
 								</div>
 							)
 					)}
@@ -71,9 +97,17 @@ class ReportResults extends Component {
 	}
 
 	componentDidMount() {
+		const userId = jwt_decode(localStorage.getItem('token')).subject;
+		console.log('userid', userId);
 		axiosWithAuth()
 			.get(`${baseURL}/responses/${this.props.match.params.reportId}`)
-			.then(res => this.setState({ responses: res.data }))
+			.then(res => {
+				const filtered = res.data[0].responses.filter(
+					response => response.userId === userId
+				);
+				console.log('filtered', filtered);
+				this.setState({ responses: res.data, filteredResponse: filtered });
+			})
 			.catch(err => console.log(err));
 	}
 
@@ -89,8 +123,8 @@ class ReportResults extends Component {
 	};
 
 	updateWithUserResponse = res => {
-		this.setState({ responses: res.data })
-	}
+		this.setState({ responses: res.data });
+	};
 }
 
 export default ReportResults;
