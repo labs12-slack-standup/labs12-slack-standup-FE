863fdcecff041c2187d346f7aad36ea2fde89047
AAsriyan <aasriyan@protonmail.com>
2019-05-21 16:05:16 -0400

    Changed profile page, added time picker components, styled onboarding

--- client/src/components/Onboarding/LandingPage.js
+++ client/src/components/Onboarding/LandingPage.js
@@ -1,23 +1,46 @@
 import React from 'react';
-import { Card, Button, Elevation } from '@blueprintjs/core';
+import {
+	Card,
+	Button,
+	Input,
+	InputLabel,
+	FormControl,
+	Fab,
+	Icon,
+	Divider
+} from '@material-ui/core';
+
+import './onboarding.css';
 
 const LandingPage = props => {
 	return (
 		<Card className="onboarding">
 			<h2>Welcome to Stand-Em-Ups!</h2>
-			<Card
-				interactive={false}
-				elevation={Elevation.FOUR}
-				className="onboardingCard"
-			>
-				<h4>Do you have a join code?</h4>
-				<div>
-					Yes:
-					<Button onClick={props.joinToggle}>Input join code</Button>
-				</div>
-				<div>
-					No:
-					<Button onClick={props.createToggle}>Create a Team</Button>
+			<Card raised={true} className="onboardingCard">
+				<div className="onboarding-card-content">
+					<h3>You can join a team or create your own!</h3>
+					<div className="landing-buttons">
+						<p>Join a team with your join code!</p>
+						<Button
+							className="landing-buttons"
+							color="primary"
+							variant="outlined"
+							onClick={props.joinToggle}
+						>
+							Input join code
+						</Button>
+					</div>
+					<Divider variant="fullWidth" />
+					<div className="landing-buttons">
+						<p>Create a new team!</p>
+						<Button
+							color="primary"
+							variant="outlined"
+							onClick={props.createToggle}
+						>
+							Create a Team
+						</Button>
+					</div>
 				</div>
 			</Card>
 		</Card>
