ec5b2f741a7443cfa4799362b7002ae094e49b16
mikaelacurrier <mikaela.currier@gmail.com>
2019-05-22 11:39:17 -0400

    took out card around submit report

--- client/src/components/Reports/ModifyReports/CreateReport.js
+++ client/src/components/Reports/ModifyReports/CreateReport.js
@@ -294,21 +294,21 @@ class CreateReport extends Component {
 								</Fab>
 							</section>
 						</section>
-						<section className="schedule-card-content">
+						{/* <section className="schedule-card-content">
 							<h3 className="schedule-title">Submit Report</h3>
 							<Divider className="divider" variant="fullWidth" />
-							<FormControl>
-								<Button
-									style={{ display: 'block', marginTop: '30px' }}
-									variant="contained"
-									color="primary"
-									onClick={this.addReport}
-									disabled={this.state.questions.length === 0 ? true : false}
-								>
-									Create Report
-								</Button>
-							</FormControl>
-						</section>
+							<FormControl> */}
+						<Button
+							style={{ display: 'block', marginTop: '30px' }}
+							variant="contained"
+							color="primary"
+							onClick={this.addReport}
+							disabled={this.state.questions.length === 0 ? true : false}
+						>
+							Create Report
+						</Button>
+						{/* </FormControl>
+						</section> */}
 					</Card>
 				</form>
 			</div>
